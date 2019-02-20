143fb9ed077f84af248d4c547909f761871848ec
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-02-20 16:48:19 +0100

    Corrected cropping file naming

--- data_processing/data_loader.py
+++ data_processing/data_loader.py
@@ -27,28 +27,28 @@ class DataLoader :
 
     def crop_unique_npy(self, scenario_id : int, index : int, res : int):
         npy = np.load(self.get_npy_path(scenario_id,index))
-        height = np.shape(npy)[1]
-        width = np.shape(npy)[0]
+        image_width = np.shape(npy)[1]
+        image_height = np.shape(npy)[0]
         if not os.path.exists(os.path.join(self.__output_directory, "scenario_"+str(scenario_id)+"_"+str(res))):
             os.makedirs(os.path.join(self.__output_directory, "scenario_"+str(scenario_id)+"_"+str(res)))
-        for i in range(int(width/res)) :
-            for j in range(int(height / res)):
+        for i in range(int(image_height/res)) :
+            for j in range(int(image_width / res)):
                 cropped = npy[res * i:res * (i + 1), res * j: res * (j + 1)]
                 np.save(os.path.join(self.__output_directory, "scenario_"+str(scenario_id)+"_"+str(res),
-                                     str(index).zfill(4)+"_"+str(i*res)+"_"+str(j*res)), cropped)
+                                     str(index).zfill(4)+"_"+str(j*res)+"_"+str(i*res)), cropped)
             #Saving last cell (shifting it to the top in order not to overpass the limit)
-            cropped = npy[res * i:res * (i + 1),height-res:height]
+            cropped = npy[res * i:res * (i + 1),image_width-res:image_width]
             np.save(os.path.join(self.__output_directory, "scenario_" + str(scenario_id) + "_" + str(res),
-                                 str(index).zfill(4) + "_" + str(i * res) + "_" + str(height-res)), cropped)
+                                 str(index).zfill(4) + "_" + str(image_width-res))+ "_"+ str(i * res), cropped)
 
         # Saving last column (shifting it to the left in order not to overpass the limit)
-        for j in range(int(height / res)):
-            cropped = npy[width-res:width, res*j:res*(j+1)]
+        for j in range(int(image_width / res)):
+            cropped = npy[image_height-res:image_height, res*j:res*(j+1)]
             np.save(os.path.join(self.__output_directory, "scenario_" + str(scenario_id) + "_" + str(res),
-                             str(index).zfill(4) + "_" + str(width - res) + "_" + str(j * res)), cropped)
-        cropped = npy[width - res:width, height - res:height]
+                             str(index).zfill(4) + "_" + str(j * res))+ "_" + str(image_height - res), cropped)
+        cropped = npy[image_height - res:image_height, image_width - res:image_width]
         np.save(os.path.join(self.__output_directory, "scenario_" + str(scenario_id) + "_" + str(res),
-                             str(index).zfill(4) + "_" + str(width - res) + "_" + str(height - res)), cropped)
+                             str(index).zfill(4) + "_" + str(image_width - res)+ "_" + str(image_height - res)), cropped)
 
 
     def crop_scenario_npy(self, scenario_id : int, res : int):
